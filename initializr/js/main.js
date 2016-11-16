d9ab937d191f398d27fe2dfebd7046cf27341dfe
karlabeni12 <karla.beni12@utexas.edu>
2016-11-15 19:35:46 -0600

    step1

---
+++ initializr/js/main.js
@@ -0,0 +1,8 @@
+function highlight() {
+  $('h1').text('Hello');
+  $('.jumbotron').addClass('highlighted');
+}
+function unhighlight() {
+  $('h1').text('Hello World');
+  $('.jumbotron').removeClass('highlighted');
+}
